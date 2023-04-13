6bada9d9b68a846ea7703a3a2f2309cd9a9cc4fe
Sam Johnson <sam@durosoft.com>
2023-04-12 21:54:26 -0400

    initial skeleton, deps

--- range_example_macros/src/lib.rs
+++ range_example_macros/src/lib.rs
@@ -1,1 +1,8 @@
+use proc_macro::TokenStream;
+// use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
 
+#[proc_macro]
+pub fn example_parse(_tokens: TokenStream) -> TokenStream {
+    quote!().into()
+}
